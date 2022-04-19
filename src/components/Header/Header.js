f4933a6480c89fcfe58ea7033327570b039d6150
Geena Jackson <gjacks303@gmail.com>
2022-04-19 17:14:16 -0600

    Create file structure for Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <button className='dream-destinations'>Dream Destinations</button>
+    </div>
+  )
+}
+
+export default Header;
