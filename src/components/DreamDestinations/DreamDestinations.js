4fd1c6e379757708af118585ea7abadcf7d447ba
Geena Jackson <gjacks303@gmail.com>
2022-04-19 17:20:18 -0600

    Set up dream destinations file

---
+++ src/components/DreamDestinations/DreamDestinations.js
@@ -0,0 +1,13 @@
+import './DreamDestinations.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const DreamDestinations = () => {
+  return (
+    <div className='dream-destinations'>
+      <h1>Saved countries go here</h1>
+    </div>
+  )
+}
+
+export default DreamDestinations;
