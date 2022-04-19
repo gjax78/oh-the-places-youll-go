0566094276957ead135eb4598a59f0f00122a7ef
Geena Jackson <gjacks303@gmail.com>
2022-04-19 17:27:43 -0600

    Create countries component structure

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import './Countries.css';
+
+const Countries = () => {
+  <div className='countries'>
+    <h1>Country name goes here</h1>
+  </div>
+}
+
+export default Countries;
