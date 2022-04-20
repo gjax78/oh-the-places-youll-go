7cf5af8750f6fb0effba125819e5cc7dbad37489
Geena Jackson <gjacks303@gmail.com>
2022-04-19 20:49:55 -0600

    Add div for styling

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,15 +1,22 @@
 import React, {useState} from 'react';
 import './CountryCard.css';
+import heart from './heart.png'
 
 const CountryCard = ({ name, flag, population, region, maps }) => {
   return (
     <div className='country-card'>
-      <h1>{name}</h1>
+      <div className='country-name-header'>
+        <h1 className='country-name'>{name}</h1>
+        <div className='add-to-destinations-button-container'>
+          <button className='add-to-destinations-button'><img src={heart}/></button>
+        </div>
+      </div>
       <img src={flag} className='flag-image'/>
-      <p>population: {population}</p>
-      <p>{region}</p>
-      <a href={maps}>Google Maps</a>
-      <button className='add-to-destinations-button'>♥️</button>
+      <div className='country-information-container'>
+        <p>population: {population}</p>
+        <p>{region}</p>
+        <a href={maps}>Google Maps</a>
+      </div>
     </div>
   )
 }
