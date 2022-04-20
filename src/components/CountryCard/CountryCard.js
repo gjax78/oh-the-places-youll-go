a47da415670a8541f8b06218d04399385673646d
Geena Jackson <gjacks303@gmail.com>
2022-04-20 09:18:22 -0600

    Create function to add favorited country on click

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,14 +1,38 @@
 import React, {useState} from 'react';
 import './CountryCard.css';
 import heart from './heart.png'
+import heartPink from './heartPink.png'
 
-const CountryCard = ({ name, flag, population, region, maps }) => {
+
+const CountryCard = ({ addCountryToDreamDestinations, name, flag, population, region, maps }) => {
+  const [isFavorited, setIsFavorited] = useState(false)
+
+  const addCountry = () => {
+    if (!isFavorited) {
+      setIsFavorited(true)
+      const favoritedCountry = {
+        name: name,
+        flag: flag,
+        population: population,
+        region: region,
+        maps: maps
+      }
+      addCountryToDreamDestinations(favoritedCountry)
+    } else {
+      setIsFavorited(false)
+    }
+  }
+
   return (
     <div className='country-card'>
       <div className='country-name-header'>
         <h1 className='country-name'>{name}</h1>
         <div className='add-to-destinations-button-container'>
-          <button className='add-to-destinations-button'><img src={heart}/></button>
+          <button className='add-to-destinations-button'
+                  onClick={() => addCountry()}
+          >
+          {isFavorited ? <img src={heartPink} className='pink'/> : <img src={heart}/>}
+          </button>
         </div>
       </div>
       <img src={flag} className='flag-image'/>
