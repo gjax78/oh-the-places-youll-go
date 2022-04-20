323b5b32e7c1e07434ea72d125c7e3a4b93837f1
Geena Jackson <gjacks303@gmail.com>
2022-04-19 19:17:34 -0600

    Create country card with props from app

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,10 +1,20 @@
 import React, {useState} from 'react';
 import './Countries.css';
+import CountryCard from '../CountryCard/CountryCard'
 
-const Countries = () => {
-  <div className='countries'>
-    <h1>Country name goes here</h1>
-  </div>
+const Countries = ({ countries }) => {
+  const countryCards = countries.map(country => {
+    return (
+      <div className='countryCard-container' key={country.ccn3}>
+      </div>
+    )
+  })
+
+  return (
+    <div className='countryCard-container'>
+      {countryCards}
+    </div>
+  )
 }
 
 export default Countries;
