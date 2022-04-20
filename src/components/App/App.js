a172d73f2ff1b84d371df8c9f9334a309221068b
Geena Jackson <gjacks303@gmail.com>
2022-04-19 19:11:07 -0600

    Log data to the console effectively

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,21 @@ import { Route, Switch } from 'react-router-dom'
 import Header from '../Header/Header'
 import Countries from '../Countries/Countries'
 import DreamDestinations from '../DreamDestinations/DreamDestinations'
+import fetchData from '../../apiCalls';
+
 
 const App = () => {
+  const [countries, setCountries] = useState([])
+
+  const fetchCountries = () => {
+    fetchData.getData()
+    .then(data => setCountries(data))
+  }
+
+  useEffect(() => {
+    fetchCountries()
+  }, [])
+
   return (
     <Switch>
       <Route exact path='/'>
