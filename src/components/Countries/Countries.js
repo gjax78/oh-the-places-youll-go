a9ba149bcb6825d120e29834aca40c25eda9e974
Geena Jackson <gjacks303@gmail.com>
2022-04-19 19:27:46 -0600

    Create country card with props to display on DOM

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -5,7 +5,16 @@ import CountryCard from '../CountryCard/CountryCard'
 const Countries = ({ countries }) => {
   const countryCards = countries.map(country => {
     return (
-      <div className='countryCard-container' key={country.ccn3}>
+      <div className='countryCard-container' key={country.cca2}>
+        <CountryCard
+          id={country.cca2}
+          name={country.name.common}
+          capital={country.capital}
+          region={country.region}
+          subregion={country.subregion}
+          language={country.languages}
+          maps={country.maps}
+        />
       </div>
     )
   })
