99f5be64f6debae8efcad26b5e9427fb61e627be
Geena Jackson <gjacks303@gmail.com>
2022-04-20 09:54:20 -0600

    Add Links to Header for dream destination page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,16 @@ import suitcase from './suitcase.png'
 
 const Header = () => {
   return (
-    <div className='header-wrapper'>
-    <Link to='/dream-destinations'>
-      <div>
-        <button className='dream-destinations-button'><img src={suitcase} className='suitcase'/></button>
+      <div className='header-wrapper'>
+      <Link to='/dream-destinations'>
+        <div>
+          <button className='dream-destinations-button'><img src={suitcase} className='suitcase'/></button>
+        </div>
+      </Link>
+        <div className='header'>
+          <h1 className='header-title'>OH THE PLACES YOU'LL GO</h1>
+        </div>
       </div>
-    </Link>
-      <div className='header'>
-        <h1 className='header-title'>OH THE PLACES YOU'LL GO</h1>
-      </div>
-    </div>
   )
 }
 
