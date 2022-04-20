013b2c981f53e9acda01a3ff6078d9872a46f82f
Geena Jackson <gjacks303@gmail.com>
2022-04-19 20:10:05 -0600

    Refactor component

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import './CountryCard.css';
 
-const CountryCard = ({ name, population, region, maps }) => {
+const CountryCard = ({ name, flag, population, region, maps }) => {
   return (
     <div className='country-card'>
       <h1>{name}</h1>
+      <img src={flag} className='flag-image'/>
       <p>population: {population}</p>
       <p>{region}</p>
       <a href={maps}>Google Maps</a>
