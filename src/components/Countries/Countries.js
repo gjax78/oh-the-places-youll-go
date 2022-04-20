224abab27adf4e3ea211dde534ec33b2a0e16f96
Geena Jackson <gjacks303@gmail.com>
2022-04-20 13:36:48 -0600

    Refactor countries component

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './Countries.css';
 import CountryCard from '../CountryCard/CountryCard'
 
-const Countries = ({ countries, addCountryToDreamDestinations }) => {
+const Countries = ({ countries, addCountryToDreamDestinations, removeFavoritedCountryFromView }) => {
   const countryCards = countries.map(country => {
     return (
       <div className='countryCard-container' key={country.cca2}>
