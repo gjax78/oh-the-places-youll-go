f8544bb2c0f0d5631da6fc0ae2f227c7b2c4868a
Geena Jackson <gjacks303@gmail.com>
2022-04-20 20:14:17 -0600

    Add try catch block for error handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,37 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Header from '../Header/Header'
 import Countries from '../Countries/Countries'
 import DreamDestinations from '../DreamDestinations/DreamDestinations'
 import Search from '../Search/Search'
+import Error from '../Error/Error'
 import fetchData from '../../apiCalls';
 
 const App = () => {
   const [countries, setCountries] = useState([])
   const [dreamDestination, setDreamDestination] = useState([])
   const [search, setSearch] = useState('')
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    const fetchItems = async () => {
-      const countriesResult = await fetchData.getData()
+      const fetchItems = async () => {
+        try {
+          const countriesResult = await fetchData.getData()
 
-      if ({search} === null) {
-        setCountries(countriesResult)
-      } else if ({ search }) {
-        setCountries(countriesResult.filter(country => country.name.common.toLowerCase().includes(search)))
+          if ({search} === null) {
+            setCountries(countriesResult)
+          } else if ({ search }) {
+            setCountries(countriesResult.filter(country => country.name.common.toLowerCase().includes(search)))
+          }
+
+        } catch(error) {
+          console.log(error.message)
+          setError(error.message)
+        }
       }
-    }
-    fetchItems()
+      fetchItems()
+
   }, [search])
 
   const addCountryToDreamDestinations = (favoritedCountry) => {
@@ -31,6 +40,7 @@ const App = () => {
 
   return (
     <Switch>
+    {error && <Redirect to='/error' />}
       <Route exact path='/'>
       <Header />
         <Search getSearch={(e) => setSearch(e)}/>
@@ -44,6 +54,11 @@ const App = () => {
         <Header />
         <DreamDestinations dreamDestination={dreamDestination}/>
       </Route>
+      <Route exact path='/error'>
+              <Error
+                error={ error }
+                />
+        </Route>
     </Switch>
   )
 }
