a1c00af7f2be97d361c643ceb992c7b502b0318f
Geena Jackson <gjacks303@gmail.com>
2022-04-22 14:41:19 -0600

    Create tests for main page to dream destinations page

--- cypress/integration/MainPage_spec.js
+++ cypress/integration/MainPage_spec.js
@@ -1,4 +1,4 @@
-describe('Dashboard inital page load', () => {
+describe('Main page inital page load', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://restcountries.com/v3.1/all', {
       statusCode: 200,
@@ -15,4 +15,58 @@ describe('Dashboard inital page load', () => {
     cy.get('*[class^="dream-destinations-button"]')
       .should('be.visible')
     })
+
+  it('should have a search bar to search countries', () => {
+    cy.get('form input[placeholder="Search destinations"]')
+      .get('form input[type="text"]')
+      .get('*[class^="controlled-search"]')
+  })
+
+  it('should display all countries on the main page', () => {
+    cy.get('*[class^="countryCard-container"]')
+      .get('*[class^="country-name"]')
+        .contains('Uruguay')
+      .get('*[class^="country-information-container"]')
+        .contains('population: 3473727')
+      .get('*[class^="country-information-container"]')
+        .contains('Americas')
+      .get('*[class^="country-information-container"]')
+        .contains('Google Maps')
+  })
+
+  it('should display all countries on the main page', () => {
+    cy.get('*[class^="countryCard-container"]')
+      .get('*[class^="country-name"]')
+        .contains('Paraguay')
+      .get('*[class^="country-information-container"]')
+        .contains('population: 7132530')
+      .get('*[class^="country-information-container"]')
+        .contains('Americas')
+      .get('*[class^="country-information-container"]')
+        .contains('Google Maps')
+  })
+
+  it('should have a button to add the country to dream destinations', () => {
+    cy.get('*[class^="add-to-destinations-button"]')
+      .should('be.visible')
+  })
+
+  it('should be able to click on the button and be directed to the dream destination page', () => {
+    cy.get('*[class^="add-to-destinations-button"]')
+    .first().click()
+    cy.get('*[class^="dream-destinations-button"]')
+    .click()
+  })
+
+  it('should display the favorited country', () => {
+    cy.get('*[class^="countryCard-container"]')
+      .get('*[class^="country-name"]')
+        .contains('Uruguay')
+      .get('*[class^="country-information-container"]')
+        .contains('population: 3473727')
+      .get('*[class^="country-information-container"]')
+        .contains('Americas')
+      .get('*[class^="country-information-container"]')
+        .contains('Google Maps')
+  })
 })
