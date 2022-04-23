449efeadfbd31c395d4b53754076668584a7cc0a
Geena Jackson <gjacks303@gmail.com>
2022-04-23 08:37:13 -0600

    Create error for dream destinations

---
+++ src/components/DreamDestinationsError/DreamDestinationsError.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DreamDestinationsError.css'
+
+const DreamDestinationsError = () => {
+  return (
+    <div className='error-container'>
+      <p>You haven't added any dream destinations. Add some countries to your bucket list!</p>
+    </div>
+  )
+}
+
+export default DreamDestinationsError;
