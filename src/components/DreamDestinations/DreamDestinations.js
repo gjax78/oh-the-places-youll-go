0f5da647c9a7c8b1cd861ffdb9dae8370453f952
Geena Jackson <gjacks303@gmail.com>
2022-04-23 07:51:39 -0600

    Add remove from destinations function

--- src/components/DreamDestinations/DreamDestinations.js
+++ src/components/DreamDestinations/DreamDestinations.js
@@ -3,13 +3,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Link } from 'react-router-dom';
 import Countries from '../Countries/Countries'
-import CountryCard from '../CountryCard/CountryCard'
+import DreamCountryCard from '../DreamCountryCard/DreamCountryCard'
 
-const DreamDestinations = ({ dreamDestination }) => {
+const DreamDestinations = ({ dreamDestination, removeFromDreamDestinations }) => {
   const dreamCountries = dreamDestination.map(dreamCountry => {
     return (
       <div className='dream-destinations' key={dreamCountry.id} >
-        <CountryCard
+        <DreamCountryCard
+        removeFromDreamDestinations={removeFromDreamDestinations}
           {...dreamCountry}
         />
       </div>
