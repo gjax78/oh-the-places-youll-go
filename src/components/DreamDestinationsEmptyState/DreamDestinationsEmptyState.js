aa708a07f29bad078b5b51f118c3a4c53c216747
Geena Jackson <gjacks303@gmail.com>
2022-04-28 19:23:03 -0600

    Refactor file name to reflect empty state vs error

---
+++ src/components/DreamDestinationsEmptyState/DreamDestinationsEmptyState.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DreamDestinationsError = () => {
+  return (
+    <div className='error-container'>
+      <p>You haven't added any dream destinations. Add some countries to your bucket list!</p>
+    </div>
+  )
+}
+
+export default DreamDestinationsError;
